d9a378b069fef31b54c7c9f06dbd15a1914b02db
madhavparekh <madhavparekh@gmail.com>
2018-02-06 16:30:48 -0800

    updated .js to take only a-z keyup event

--- assets/js/startTheGame.js
+++ assets/js/startTheGame.js
@@ -80,60 +80,65 @@ function onButtonClick(){
     document.getElementById("hangManPic").src = "assets/images/hm1.png"; 
 
     document.onkeyup = function(event){
-        
+
         if(!disableInput){
-            var pressedKey = String.fromCharCode(event.which).toUpperCase();
+            if((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)){
+                var pressedKey = String.fromCharCode(event.which).toUpperCase();
 
-            //check if char was already gusessed
-            if(nextWord.guessedChar.indexOf(pressedKey) !== -1){
-                alert('"' +pressedKey + ' already been entered. Try another character');
-            }
-            else{
-                //enter char into guessedChar
-                nextWord.guessedChar += pressedKey;
-
-                //check if char in word
-                if(nextWord.updateJumble(pressedKey)){
-                    document.getElementById("correct").play();
-                    document.getElementById("dispJumble").textContent = nextWord.updatedJumble;
-                }    
-                else{
-                    
-                    document.getElementById("wrong").play();
-                    nextWord.wrongChar += pressedKey;
-                    document.getElementById("wrongChar").textContent = nextWord.wrongChar;
-                    nextWord.wrongChar += ", ";
-                    document.getElementById("guessesLeft").textContent = --nextWord.guessesLeft;
-                    //updating hangman pic
-                    document.getElementById("hangManPic").src = "assets/images/hm" + (9 - nextWord.guessesLeft) + ".png";
+                //check if char was already gusessed
+                if(nextWord.guessedChar.indexOf(pressedKey) !== -1){
+                    alert('"' +pressedKey + ' already been entered. Try another character');
                 }
+                else{
+                    //enter char into guessedChar
+                    nextWord.guessedChar += pressedKey;
 
-                console.log("lenght of nextWord.currectGuesses :" +nextWord.correctGuesses );
+                    //check if char in word
+                    if(nextWord.updateJumble(pressedKey)){
+                        document.getElementById("correct").play();
+                        document.getElementById("dispJumble").textContent = nextWord.updatedJumble;
+                    }    
+                    else{
+                        
+                        document.getElementById("wrong").play();
+                        nextWord.wrongChar += pressedKey;
+                        document.getElementById("wrongChar").textContent = nextWord.wrongChar;
+                        nextWord.wrongChar += ", ";
+                        document.getElementById("guessesLeft").textContent = --nextWord.guessesLeft;
+                        //updating hangman pic
+                        document.getElementById("hangManPic").src = "assets/images/hm" + (9 - nextWord.guessesLeft) + ".png";
+                    }
 
-                if(nextWord.correctGuesses === nextWord.word.length){
-                    //On winning play music and display GIF
-                    if(nextWord.checkIfSolved()){
-                        document.getElementById("winner").play();
-                        console.log("Yay Solved!");
-                        document.getElementById("hangManPic").src = "assets/images/win.gif";
-                        document.getElementById("wins").textContent = ++wins;
+                    console.log("lenght of nextWord.currectGuesses :" +nextWord.correctGuesses );
+
+                    if(nextWord.correctGuesses === nextWord.word.length){
+                        //On winning play music and display GIF
+                        if(nextWord.checkIfSolved()){
+                            document.getElementById("winner").play();
+                            console.log("Yay Solved!");
+                            document.getElementById("hangManPic").src = "assets/images/win.gif";
+                            document.getElementById("wins").textContent = ++wins;
+                            document.getElementById("startGame").textContent = "Play Again";
+                            document.getElementById("startGame").disabled = false;
+                        }
+                        //disableling keyboard
+                        disableInput = true;
+                        
+                    } else if(nextWord.guessesLeft < 1) {
+                        //On losing play music and display GIF
+                        document.getElementById("loser").play();
+                        console.log("Not Solved!");
+                        document.getElementById("hangManPic").src = "assets/images/lose.gif";
+                        document.getElementById("loses").textContent = ++loses;
                         document.getElementById("startGame").textContent = "Play Again";
                         document.getElementById("startGame").disabled = false;
+                        //disableling keyboard
+                        disableInput = true;
                     }
-                    //disableling keyboard
-                    disableInput = true;
-                    
-                } else if(nextWord.guessesLeft < 1) {
-                    //On losing play music and display GIF
-                    document.getElementById("loser").play();
-                    console.log("Not Solved!");
-                    document.getElementById("hangManPic").src = "assets/images/lose.gif";
-                    document.getElementById("loses").textContent = ++loses;
-                    document.getElementById("startGame").textContent = "Play Again";
-                    document.getElementById("startGame").disabled = false;
-                    //disableling keyboard
-                    disableInput = true;
                 }
+            }
+            else{
+                alert('Enter keys between "a" thru "z" only');
             }
         }
         else{
